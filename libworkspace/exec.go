8f21ec83b24e23b3317e1809b7280f81b0eb11a3
hwang381 <wanghanjie2012@gmail.com>
2020-05-01 23:54:24 -0700

    post switch commands

--- libworkspace/exec.go
+++ libworkspace/exec.go
@@ -10,31 +10,63 @@ import (
 	"github.com/kvz/logstreamer"
 )
 
-func executePostSwitch(repo Repository) error {
-	if repo.PostSwitch == nil || len(repo.PostSwitch) == 0 {
-		log.Printf("no post switch action to be executed")
-		return nil
-	}
-	stdoutStreamer := logstreamer.NewLogstreamerForStdout(fmt.Sprintf("[%v][stdout]", repo.ID))
+func executePostSwitchCommand(repoId, repoPath string, command Command) error {
+	stdoutStreamer := logstreamer.NewLogstreamerForStdout(fmt.Sprintf("[%v][stdout]", repoId))
 	defer func() {
 		err := stdoutStreamer.Close()
 		if err != nil {
 			log.Printf("fail to close stdout stream, %v", err)
 		}
 	}()
-	stderrStreamer := logstreamer.NewLogstreamerForStderr(fmt.Sprintf("[%v][stderr]", repo.ID))
+	stderrStreamer := logstreamer.NewLogstreamerForStderr(fmt.Sprintf("[%v][stderr]", repoId))
 	defer func() {
 		err := stderrStreamer.Close()
 		if err != nil {
 			log.Printf("fail to close stderr stream, %v", err)
 		}
 	}()
-	cmd := exec.Command(repo.PostSwitch[0], repo.PostSwitch[1:]...)
+	cmd := exec.Command(command.Exe[0], command.Exe[1:]...)
 	cmd.Env = os.Environ()
-	cmd.Dir = repo.Path
+	if command.Environ != nil {
+		for key, value := range command.Environ {
+			cmd.Env = append(cmd.Env, fmt.Sprintf("%s=%s", key, value))
+		}
+	}
+	cmd.Dir = repoPath
 	cmd.Stdout = stdoutStreamer
 	cmd.Stderr = stderrStreamer
 	return cmd.Run()
+}
+
+func executePostSwitch(repo Repository) error {
+	var commands []Command
+	if repo.PostSwitchCommands != nil {
+		commands = repo.PostSwitchCommands
+	} else if repo.PostSwitch != nil && len(repo.PostSwitch) != 0 {
+		commands = []Command{
+			{
+				Exe: repo.PostSwitch,
+			},
+		}
+	}
+
+	if len(commands) == 0 {
+		log.Printf("no post switch action to be executed")
+		return nil
+	}
+
+	for _, command := range commands {
+		err := executePostSwitchCommand(
+			repo.ID,
+			repo.Path,
+			command,
+		)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func executePostSwitchAsync(repo Repository, wg *sync.WaitGroup) error {
--- libworkspace/models.go
+++ libworkspace/models.go
@@ -1,18 +1,18 @@
 package libworkspace
 
 type Repository struct {
-	ID           string   `json:"id"`
-	Path         string   `json:"path"`
-	Dependencies []string `json:"dependencies"`
-	PostSwitch   []string `json:"postSwitch,omitEmpty"`
+	ID                 string    `json:"id"`
+	Path               string    `json:"path"`
+	Dependencies       []string  `json:"dependencies"`
+	PostSwitch         []string  `json:"postSwitch,omitEmpty"`
+	PostSwitchCommands []Command `json:"postSwitchCommands,omitEmpty"`
 }
 
-type GitConfig struct {
-	PrivateKeyPath    string `json:"privateKeyPath"`
-	KeyPairPassphrase string `json:"keyPairPassphrase"`
+type Command struct {
+	Exe     []string          `json:"exe"`
+	Environ map[string]string `json:"environ,omitEmpty"`
 }
 
 type Config struct {
 	Repositories []Repository `json:"repositories"`
-	GitConfig    GitConfig    `json:"gitConfig"`
 }
